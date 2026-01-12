90b8d09e02b76ca734fef0e8c92740b76739fb0a
gaurav2361 <gaurav23617@gmail.com>
2026-01-12 16:52:25 +0530

    feat: ft plugin some new filetypes added

--- plugin/ft.lua
+++ plugin/ft.lua
@@ -3,13 +3,16 @@ vim.filetype.add({
   extension = {
     mdx = "mdx",
     env = "dotenv",
+    base = "yaml",
   },
   filename = {
     [".env"] = "sh",
     [".envrc"] = "sh",
     ["*.env"] = "sh",
+    ["*.env.*"] = "sh",
     ["*.envrc"] = "sh",
     ["env"] = "dotenv",
+    [".base"] = "yaml",
   },
   pattern = {
     ["[jt]sconfig.*.json"] = "jsonc",
