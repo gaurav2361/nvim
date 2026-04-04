2dd89402c01758605dd3d2d45c94c5d496df8d04
gaurav2361 <gaurav23617@gmail.com>
2026-04-04 13:50:54 +0530

    fix(lsp): replace deprecated vim.lsp.with in handlers

--- lua/kiyo/plugins/lsp.lua
+++ lua/kiyo/plugins/lsp.lua
@@ -164,9 +164,9 @@ return {
     -- Handlers Configuration
     -- Customize LSP floating windows
     vim.lsp.handlers["textDocument/hover"] =
-      vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+      vim.lsp.handlers.hover({ border = "rounded" })
 
     vim.lsp.handlers["textDocument/signatureHelp"] =
-      vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+      vim.lsp.handlers.signature_help({ border = "rounded" })
   end,
 }
